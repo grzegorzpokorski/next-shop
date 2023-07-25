ee024cf8c5ad0c42fbecc58952837375569cbfa0
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-07-25 16:52:26 +0200

    feat: add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ variable: "--font-poppins", subsets: ['latin', 'latin-ext'], weight: ["400", "700"], })
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin", "latin-ext"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
-  title: 'Next shop',
-  description: 'Next stunning shop written using next.js',
-}
+  title: "Next shop",
+  description: "Next stunning shop written using next.js",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pl">
       <body className={`${poppins.variable}`}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <p>hello world</p>
-  )
+  return <p>hello world</p>;
 }
