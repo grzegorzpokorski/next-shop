580f0fbb21a8d5971c7fc7baf6d08bf162f04c47
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-07-25 16:55:28 +0200

    fix: fix tailwind content paths and add fontFamily variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      typography: {
+        fontFamily: {
+          sans: ["var(--font-poppins)", ...defaultTheme.fontFamily.sans],
+        },
       },
     },
   },
