821d003a12280c6655a2fbc4ca33539db40c7cb6
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-07-29 02:51:24 +0200

    feat: add ability to visually hide Container

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -1,14 +1,19 @@
 import type { AriaAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   children: ReactNode;
   as: keyof Pick<JSX.IntrinsicElements, "div" | "section" | "header">;
   id?: string;
+  hidden?: boolean;
 } & Pick<AriaAttributes, "aria-labelledby">;
 
-export const Container = ({ as: As, children, ...props }: Props) => {
+export const Container = ({ as: As, children, hidden, ...props }: Props) => {
   return (
-    <As className="container mx-auto px-3 lg:px-6" {...props}>
+    <As
+      className={twMerge("container mx-auto px-3 lg:px-6", hidden && "sr-only")}
+      {...props}
+    >
       {children}
     </As>
   );
