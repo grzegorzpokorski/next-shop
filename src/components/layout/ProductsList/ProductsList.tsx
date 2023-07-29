5c756ecfde38e68435c896d8210255caf547328a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-07-29 02:55:36 +0200

    refactor: refactor ProductsList by creating Product component

--- src/components/layout/ProductsList/ProductsList.tsx
+++ src/components/layout/ProductsList/ProductsList.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
+import { Product } from "@/components/layout/ProductsList/components/Product/Product";
 import type data from "@/data/data.json";
-import { formatPrice } from "@/utils/formatPrice";
 
 type Props = {
   products: (typeof data)[number][];
@@ -15,37 +13,7 @@ export const ProductsList = ({ products }: Props) => {
     >
       {products.length > 0 &&
         products.map((product) => {
-          return (
-            <li
-              key={product.id}
-              className="aspect-square transition-opacity motion-reduce:transition-none motion-safe:animate-fadeIn"
-            >
-              <Link
-                href={`/product/${product.slug}`}
-                className="inline-block w-full h-full"
-              >
-                <div className="flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-indigo-600 dark:hover:border-indigo-500 dark:bg-black relative border-neutral-200 dark:border-neutral-800">
-                  <Image
-                    src={product.image.url}
-                    width={product.image.width}
-                    height={product.image.height}
-                    alt={product.image.alt}
-                    className="relative h-full w-full object-contain transition motion-reduce:transition-none duration-300 ease-in-out motion-safe:hover:scale-105"
-                  />
-                  <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label">
-                    <div className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black backdrop-blur-md @[275px]/label:text-xs dark:border-neutral-800 dark:bg-black/70 dark:text-white">
-                      <h3 className="mr-4 inline pl-2 leading-none tracking-tight">
-                        {product.name}
-                      </h3>
-                      <p className="flex-none rounded-full bg-indigo-600 p-2 text-white">
-                        {formatPrice(product.price)}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </li>
-          );
+          return <Product key={product.id} product={product} />;
         })}
     </ul>
   );
---
+++ src/components/layout/ProductsList/components/Product/Product.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
+import { formatPrice } from "@/utils/formatPrice";
+import type data from "@/data/data.json";
+
+type Props = {
+  product: (typeof data)[number];
+};
+
+export const Product = ({ product }: Props) => {
+  return (
+    <li
+      key={product.id}
+      className="aspect-square transition-opacity motion-reduce:transition-none motion-safe:animate-fadeIn"
+    >
+      <Link
+        href={`/product/${product.slug}`}
+        className="inline-block w-full h-full"
+      >
+        <div className="flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-indigo-600 dark:hover:border-indigo-500 dark:bg-black relative border-neutral-200 dark:border-neutral-800">
+          <Image
+            src={product.image.url}
+            width={product.image.width}
+            height={product.image.height}
+            alt={product.image.alt}
+            className="relative h-full w-full object-contain transition motion-reduce:transition-none duration-300 ease-in-out motion-safe:hover:scale-105"
+          />
+          <div className="absolute bottom-0 left-0 right-0 flex w-full px-4 pb-4 @container/label">
+            <div className="flex w-full gap-4 items-center rounded border bg-white/70 p-1 font-medium text-black backdrop-blur-md text-xs dark:border-neutral-800 dark:bg-black/70 dark:text-white">
+              <h3 className="inline leading-none tracking-tight mr-auto pl-2">
+                {product.name}
+              </h3>
+              <p className="flex-none rounded bg-indigo-600 p-2 text-white">
+                {formatPrice(product.price)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+};
