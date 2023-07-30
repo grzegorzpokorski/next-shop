09bfef0af7670c7198ba7a9be6b6066597d5143f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-07-30 17:27:57 +0200

    feat: add Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,29 @@
+import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import "../src/styles/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+    darkMode: {
+      darkClass: "dark",
+      lightClass: "light",
+      classTarget: "html",
+      stylePreview: true,
+      dark: { ...themes.dark },
+      light: { ...themes.normal },
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
