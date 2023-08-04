1965b6de01b7c928563b168e63961690812ee140
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-04 15:16:14 +0200

    fix: adjust border-color in button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
         "outline-active":
-          "border border-neutral-500 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-500 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
+          "border border-neutral-500 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-600 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
         secondary:
           "bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80",
         ghost:
