253f7508e5e653d299a5f41d2a6b5245c4238ace
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-04 03:27:14 +0200

    refactor: remove temp pages for categories

--- src/app/keyboards/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Keyboards } from "@/components/pages/Keyboards/Keyboards";
-
-export default function Page() {
-  return <Keyboards />;
-}
--- src/app/mice/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Mice } from "@/components/pages/Mice/Mice";
-
-export default function Page() {
-  return <Mice />;
-}
