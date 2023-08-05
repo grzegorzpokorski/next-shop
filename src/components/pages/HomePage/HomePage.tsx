a84ffee72f0f7fd9f0331cdf28e2dfabb84b18ce
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-05 14:48:24 +0200

    refactor: rename component/pages/Home => components/pages/HomePage

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Container } from "@/components/ui/Container/Container";
 
-export const Home = () => {
+export const HomePage = () => {
   return (
     <Container as="section">
       <p className="my-4">home - tu będą bannery</p>
