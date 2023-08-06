a1c0f7d729d60ea352dd527eca8ed6069ca5d9fc
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-06 16:38:43 +0200

    refactor: move ProductListItem component into ProductList file

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
-import { ProductListItem } from "@/components/ProductListItem/ProductListItem";
+import { formatPrice } from "@/utils/formatPrice";
 import type { ProductSummaryFragment } from "@/generated/graphql";
 
-type Props = {
+type ProductsListProps = {
   products: ProductSummaryFragment[];
   slider?: boolean;
 };
 
-export const ProductsList = ({ products, slider }: Props) => {
+export const ProductsList = ({ products, slider }: ProductsListProps) => {
   return (
     <ul
       className={twMerge(
@@ -23,5 +25,55 @@ export const ProductsList = ({ products, slider }: Props) => {
           return <ProductListItem key={product.id} product={product} slider />;
         })}
     </ul>
+  );
+};
+
+type ProductListItemProps = {
+  product: ProductSummaryFragment;
+  slider?: boolean;
+};
+
+const ProductListItem = ({ product, slider }: ProductListItemProps) => {
+  return (
+    <li
+      key={product.id}
+      className={twMerge(
+        "aspect-square transition-opacity motion-reduce:transition-none motion-safe:animate-fadeIn w-full",
+        slider && "min-w-[280px]",
+      )}
+    >
+      <Link
+        href={`/product/${product.slug}`}
+        className="inline-block w-full h-full"
+      >
+        <div className="flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-indigo-600 dark:hover:border-indigo-500 dark:bg-black relative border-neutral-200 dark:border-neutral-800">
+          <Image
+            src={product.gallery[0].url}
+            width={product.gallery[0].width || 0}
+            height={product.gallery[0].height || 0}
+            alt={product.gallery[0].fileName}
+            className="relative h-full w-full object-contain transition motion-reduce:transition-none duration-300 ease-in-out motion-safe:hover:scale-105"
+            unoptimized
+          />
+          <div className="absolute bottom-0 left-0 right-0 flex w-full px-4 pb-4">
+            <div className="flex w-full gap-4 items-center rounded border bg-white/70 p-1 font-medium text-black backdrop-blur-md text-xs dark:border-neutral-800 dark:bg-black/70 dark:text-white">
+              <h3 className="inline leading-none tracking-tight mr-auto pl-2">
+                {product.name}
+              </h3>
+              <p
+                className={twMerge(
+                  "flex-none rounded bg-indigo-600 p-2 text-white",
+                )}
+              >
+                {formatPrice({
+                  price: product.price,
+                  currency: product.currency,
+                })}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
   );
 };
