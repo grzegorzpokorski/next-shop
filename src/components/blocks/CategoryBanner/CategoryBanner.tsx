55f1f490e02e261a05d2e413f852e75dc50d4a61
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-11 01:07:23 +0200

    feat: add CategoryBanner component

--- src/components/blocks/CategoryBanner/CategoryBanner.tsx
+++ src/components/blocks/CategoryBanner/CategoryBanner.tsx
@@ -1,25 +1,34 @@
 import Link from "next/link";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 import type { Image as ImageType } from "@/lib/types";
 import { Heading } from "@/components/ui/Heading/Heading";
 
 type Props = {
   link: string;
-  thumbnail: ImageType;
+  thumbnail?: ImageType;
   title: string;
+  fullWidth?: boolean;
 };
 
-export const CategoryBanner = ({ link, thumbnail, title }: Props) => {
+export const CategoryBanner = ({
+  link,
+  thumbnail,
+  title,
+  fullWidth,
+}: Props) => {
   return (
-    <Link href={link}>
+    <Link href={link} className={twMerge(fullWidth && "lg:col-span-2")}>
       <div className="bg-black min-h-[280px] lg:min-h-[320px] rounded-md overflow-hidden relative border-2 hover:border-indigo-500 motion-safe:transition-colors group">
-        <Image
-          src={thumbnail.url}
-          alt={thumbnail.alt}
-          width={thumbnail.width}
-          height={thumbnail.height}
-          className="absolute h-full w-full object-cover"
-        />
+        {thumbnail && (
+          <Image
+            src={thumbnail.url}
+            alt={thumbnail.alt}
+            width={thumbnail.width}
+            height={thumbnail.height}
+            className="absolute h-full w-full object-cover transition ease-in-out duration-300 motion-safe:group-hover:scale-105"
+          />
+        )}
         <div className="absolute inset-0 bg-gradient-to-t from-neutral-900 dark:from-black/80 flex flex-col justify-end p-16 text-white">
           <Heading as="h3" size="4xl">
             {title}
