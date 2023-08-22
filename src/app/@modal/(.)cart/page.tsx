a48f7d319c1136ab0785a60a8fd3d110b499f116
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-22 14:17:28 +0200

    refactor/feat: add border to cart modal, move cart content styles to CartModal component

--- src/app/@modal/(.)cart/page.tsx
+++ src/app/@modal/(.)cart/page.tsx
@@ -9,16 +9,14 @@ export default async function Page() {
   const { products } = await getAllProducts({ limit: 5, skip: 0 });
   return (
     <CartModal>
-      <div className="w-full max-w-screen-sm fixed top-0 right-0 bottom-0 bg-white dark:bg-neutral-900 overflow-y-auto z-50">
-        <div className="sticky top-0 flex py-4 px-3 md:px-8 items-center justify-between border-b bg-white/60 dark:bg-neutral-900/60 backdrop-blur-lg z-50">
-          <Heading as="h2" size="2xl">
-            Twój koszyk
-          </Heading>
-          <CloseCartButton />
-        </div>
-        <CartItemsList products={products} />
-        <CartSummary total={{ amount: 998, currency: "PLN" }} sticky />
+      <div className="sticky top-0 flex py-4 px-3 md:px-8 items-center justify-between border-b bg-white/60 dark:bg-neutral-900/60 backdrop-blur-lg z-50">
+        <Heading as="h2" size="2xl">
+          Twój koszyk
+        </Heading>
+        <CloseCartButton />
       </div>
+      <CartItemsList products={products} />
+      <CartSummary total={{ amount: 998, currency: "PLN" }} sticky />
     </CartModal>
   );
 }
--- src/components/layout/Cart/CartModal/CartModal.tsx
+++ src/components/layout/Cart/CartModal/CartModal.tsx
@@ -15,7 +15,9 @@ export const CartModal = ({ children }: Props) => {
     <Dialog.Root defaultOpen={true} onOpenChange={() => router.back()}>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-white/50 dark:bg-black/50 z-50 backdrop-blur-lg" />
-        <Dialog.Content>{children}</Dialog.Content>
+        <Dialog.Content className="w-full max-w-screen-sm fixed top-0 right-0 bottom-0 bg-white dark:bg-neutral-900 overflow-y-auto z-50 border-l">
+          {children}
+        </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   );
