a7306a645763ea50406ca38223e944d6db4e3d95
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-29 21:44:06 +0200

    feat: add loading.tsx for product page

---
+++ src/app/product/[slug]/loading.tsx
@@ -0,0 +1,43 @@
+import { Container } from "@/components/ui/Container/Container";
+import { Skeleton } from "@/components/ui/Skeleton/Skeleton";
+import { env } from "@/lib/env.mjs";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-8 py-8">
+      <Container as="article" aria-labelledby="product-title">
+        <div className="flex flex-col md:flex-row gap-8 bg-white dark:bg-black p-8 rounded-md border">
+          <div className="flex flex-col h-full w-full md:w-1/2 overflow-hidden md:sticky md:top-20">
+            <Skeleton className="aspect-square h-full w-full" />
+          </div>
+          <div className="flex flex-col items-start justify-center gap-4 w-full md:w-1/2">
+            <Skeleton className="w-20 h-6 rounded-full" />
+            <Skeleton className="w-full lg:w-[80%] h-10 mb-4" />
+            <Skeleton className="w-32 h-8" />
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-[80%] h-4" />
+            <Skeleton className="w-56 h-11" />
+          </div>
+        </div>
+        <div className="mt-8">
+          <div className="flex flex-col gap-2">
+            <Skeleton className="w-80 h-8" />
+          </div>
+          <div className="list-none py-4 gap-3 md:gap-4 flex flex-row overflow-x-auto">
+            {Array(env.PRODUCTS_PER_PAGE)
+              .fill(0)
+              .map((_, idx) => (
+                <Skeleton
+                  key={idx}
+                  className="aspect-square h-full min-w-[280px]"
+                />
+              ))}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
