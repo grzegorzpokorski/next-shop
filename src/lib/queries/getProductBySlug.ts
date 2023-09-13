b684970f832ccfdcbc285653554cbeac92bcf726
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-09-13 23:39:34 +0200

    feat: set force-cache for each product page

--- src/lib/queries/getProductBySlug.ts
+++ src/lib/queries/getProductBySlug.ts
@@ -1,6 +1,7 @@
 import { GetProductBySlugDocument } from "@/lib/generated/graphql";
 import { fetcher } from "@/lib/fetcher";
 import { reshapeProductWithDetails } from "@/lib/mappers";
+import { TAGS } from "@/lib/constants";
 
 type Args = {
   slug: string;
@@ -10,6 +11,8 @@ export const getProductBySlug = async ({ slug }: Args) => {
   const result = await fetcher({
     query: GetProductBySlugDocument,
     variables: { slug },
+    cache: "force-cache",
+    tags: [TAGS.products],
   });
 
   return result.product ? reshapeProductWithDetails(result.product) : null;
