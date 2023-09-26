201aed2be565d42e06ab623ec6a39ff3805a9412
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-09-26 13:42:54 +0200

    refactor: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsConfigPaths(), react()],
   test: {
     globals: true,
     environment: "jsdom",
