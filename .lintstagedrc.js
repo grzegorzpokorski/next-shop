1648a2f8e702f0f7fefeba06b9bedcc722037f13
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-09-27 20:14:26 +0200

    fix: fix lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
 module.exports = {
-  "*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}": (filenames) => [
-    `pnpm prettier --write ${filenames.join(" ")}`,
-  ],
-  ".{js,jsx,ts,tsx}": (filenames) =>
-    `next lint --fix --quiet ${filenames
-      .map((f) => path.relative(process.cwd(), f))
-      .join(" --file ")}`,
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}": `prettier --write --ignore-unknown`,
 };
