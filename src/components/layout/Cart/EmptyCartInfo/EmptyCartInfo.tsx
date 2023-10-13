0cf697f8510457c3bc238b48b32d9f51d5a337ef
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-13 18:12:36 +0200

    fix: fix link in empty cart

--- src/components/layout/Cart/EmptyCartInfo/EmptyCartInfo.tsx
+++ src/components/layout/Cart/EmptyCartInfo/EmptyCartInfo.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/Button/Button";
 
 export const EmptyCartInfo = () => {
@@ -5,7 +6,7 @@ export const EmptyCartInfo = () => {
     <div className="flex flex-col h-full items-center justify-center gap-4 py-8">
       <p>Twój koszyk jest pusty</p>
       <Button asChild variant="indigo" size="lg">
-        <a href="/products">Zobacz naszą ofertę</a>
+        <Link href="/products">Zobacz naszą ofertę</Link>
       </Button>
     </div>
   );
