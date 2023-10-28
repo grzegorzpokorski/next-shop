e0b38aa8012ecc55061c3f9905b94bf9e9e85b22
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-28 18:12:57 +0200

    chore: update all deps, upgrade to Next.js 14

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import "./src/lib/env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [{ hostname: "media.graphassets.com", protocol: "https" }],
     unoptimized: process.env.VERCEL_ENV !== "production",
--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,5 +1,5 @@
 import type { NextRequest } from "next/server";
-import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
