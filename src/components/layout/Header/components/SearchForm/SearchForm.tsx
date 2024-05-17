7166ab8340698588be92599e8b3384cdcfdafbaf
Grzegorz Pokorski <mr.pokorski@gmail.com>
2024-05-17 14:49:51 +0200

    chore/feat: wrap search form into search html tag (#5)

--- src/components/layout/Header/components/SearchForm/SearchForm.tsx
+++ src/components/layout/Header/components/SearchForm/SearchForm.tsx
@@ -10,46 +10,45 @@ export const SearchForm = () => {
   const inputId = useId();
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className="w-full lg:min-w-[200px] lg:max-w-[400px] relative flex"
-    >
-      <label className="sr-only" htmlFor={inputId}>
-        Szukaj produktów
-      </label>
-      <input
-        name="q"
-        type="text"
-        placeholder="Szukaj produktów..."
-        className="h-11 pr-24 w-full rounded-lg border bg-white dark:bg-neutral-800 px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:text-white dark:placeholder:text-neutral-400"
-        id={inputId}
-        value={searchQuery}
-        autoComplete="off"
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <div className="absolute right-0">
-        {searchQuery.length > 0 && (
+    <search className="w-full lg:min-w-[200px] lg:max-w-[400px]">
+      <form onSubmit={onSubmit} className="relative flex">
+        <label className="sr-only" htmlFor={inputId}>
+          Szukaj produktów
+        </label>
+        <input
+          name="q"
+          type="text"
+          placeholder="Szukaj produktów..."
+          className="h-11 pr-24 w-full rounded-lg border bg-white dark:bg-neutral-800 px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:text-white dark:placeholder:text-neutral-400"
+          id={inputId}
+          value={searchQuery}
+          autoComplete="off"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="absolute right-0">
+          {searchQuery.length > 0 && (
+            <Button
+              type="reset"
+              size="icon"
+              variant="search"
+              onClick={() => setSearchQuery("")}
+              className=""
+            >
+              <span className="sr-only">wyczyść pole wyszukiwania</span>
+              <FaPlus className="rotate-45" />
+            </Button>
+          )}
           <Button
-            type="reset"
+            type="submit"
             size="icon"
             variant="search"
-            onClick={() => setSearchQuery("")}
-            className=""
+            disabled={searchQuery.length === 0}
           >
-            <span className="sr-only">wyczyść pole wyszukiwania</span>
-            <FaPlus className="rotate-45" />
+            <span className="sr-only">szukaj</span>
+            <FaSearch />
           </Button>
-        )}
-        <Button
-          type="submit"
-          size="icon"
-          variant="search"
-          disabled={searchQuery.length === 0}
-        >
-          <span className="sr-only">szukaj</span>
-          <FaSearch />
-        </Button>
-      </div>
-    </form>
+        </div>
+      </form>
+    </search>
   );
 };
