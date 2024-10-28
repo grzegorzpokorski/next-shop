ad77e3417263317c9c507bbb3b5cddd69083d592
Grzegorz Pokorski <mr.pokorski@gmail.com>
2024-10-28 12:42:22 +0100

    feat: use Form from next/form in search form

--- src/components/layout/Header/components/SearchForm/SearchForm.tsx
+++ src/components/layout/Header/components/SearchForm/SearchForm.tsx
@@ -1,17 +1,19 @@
 "use client";
 
+import Form from "next/form";
 import { useId } from "react";
-import { FaPlus, FaSearch } from "react-icons/fa";
-import { useSearchForm } from "./hooks/useSearchForm";
+import { FaSearch } from "react-icons/fa";
+import { useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/Button/Button";
 
 export const SearchForm = () => {
-  const { onSubmit, searchQuery, setSearchQuery } = useSearchForm();
   const inputId = useId();
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams?.get("q");
 
   return (
     <search className="w-full lg:min-w-[200px] lg:max-w-[400px]">
-      <form onSubmit={onSubmit} className="relative flex">
+      <Form action="/products" className="relative flex">
         <label className="sr-only" htmlFor={inputId}>
           Szukaj produktów
         </label>
@@ -21,34 +23,16 @@ export const SearchForm = () => {
           placeholder="Szukaj produktów..."
           className="h-11 pr-24 w-full rounded-lg border bg-white dark:bg-neutral-800 px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:text-white dark:placeholder:text-neutral-400"
           id={inputId}
-          value={searchQuery}
           autoComplete="off"
-          onChange={(e) => setSearchQuery(e.target.value)}
+          defaultValue={searchTerm ?? ""}
         />
         <div className="absolute right-0">
-          {searchQuery.length > 0 && (
-            <Button
-              type="reset"
-              size="icon"
-              variant="search"
-              onClick={() => setSearchQuery("")}
-              className=""
-            >
-              <span className="sr-only">wyczyść pole wyszukiwania</span>
-              <FaPlus className="rotate-45" />
-            </Button>
-          )}
-          <Button
-            type="submit"
-            size="icon"
-            variant="search"
-            disabled={searchQuery.length === 0}
-          >
+          <Button type="submit" size="icon" variant="search">
             <span className="sr-only">szukaj</span>
             <FaSearch />
           </Button>
         </div>
-      </form>
+      </Form>
     </search>
   );
 };
